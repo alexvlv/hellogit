ef178aed8d922678071e5885c66f45a30b3d4f13
Alex <alexey.v.v@mail.ru>
2020-12-04 23:58:20 +0300

    SVN header added

---
+++ svn_header.h
@@ -0,0 +1,26 @@
+/**************************************************************************
+$Id:  $
+$HeadURL:  $
+$LastChangedRevision:  $
+$LastChangedDate:  $
+$LastChangedBy:  $
+***************************************************************************/
+
+/*
+Id HeadURL LastChangedRevision LastChangedDate LastChangedBy
+*/
+
+/*
+ 
+#
+# $Id:  $
+# $HeadURL:  $
+# $LastChangedRevision:  $
+# $LastChangedDate:  $
+# $LastChangedBy:  $
+#
+ 
+ 
+ */
+ 
+svn ps svn:keywords "Id HeadURL LastChangedRevision LastChangedDate LastChangedBy" *.c*
