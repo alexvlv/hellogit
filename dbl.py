c8fd6e33c8f61c8f0275afc4fa56611bd6e68648
Alex <alexey.v.v@mail.ru>
2020-12-06 21:42:55 +0300

    next  to develop (rebase test will be) 3

--- dbl.py
+++ dbl.py
@@ -26,4 +26,13 @@ for line in strings:
         print( "[{}] skipped".format(line[0:40]))
         continue
     print( "[{}] len:{}".format(line,len(line)))
+	
+# for next commit after 
+# Author:			Alex <alexey.v.v@mail.ru>
+#  Date:			1 minute ago (06.12.2020 21:39:21)
+#Commit hash:	e6898aac726c67d6fd467afb5b8ee4e026201cfc
+#Child:			Commit index
+#Parent:			2a355055
+# after Id d718b29c7911d9b38bb22fe1a0091f51b829b658	
+	
 
